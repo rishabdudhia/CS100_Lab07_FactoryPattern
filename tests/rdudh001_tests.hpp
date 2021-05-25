6a82e223893d479984fc56c7cd992f5e795a3f25
rishabdudhia <rishabdudhia@gmail.com>
2021-05-24 21:49:43 -0700

    Working on tests

---
+++ tests/rdudh001_tests.hpp
@@ -0,0 +1,6 @@
+#ifndef __RDUDH001_HPP__
+#define __RDUDH001_HPP__
+
+
+
+#endif
--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -1,5 +1,17 @@
 #include "gtest/gtest.h"
 
+#include "../header/base.hpp"
+#include "../header/add.hpp"
+#include "../header/pow.hpp"
+#include "../header/op.hpp"
+#include "../header/sub.hpp"
+#include "../header/mult.hpp"
+#include "../header/div.hpp"
+#include "../header/rand.hpp"
+#include "../header/factory.hpp"
+
+#include "rdudh001_tests.hpp"
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
