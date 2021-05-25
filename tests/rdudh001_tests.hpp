2b9ca85e663115e3c6afed3cae1e9b1cc4455a93
rishabdudhia <rishabdudhia@gmail.com>
2021-05-24 22:00:04 -0700

    First test

--- tests/rdudh001_tests.hpp
+++ tests/rdudh001_tests.hpp
@@ -1,6 +1,22 @@
 #ifndef __RDUDH001_HPP__
 #define __RDUDH001_HPP__
 
+#include "../header/base.hpp"
+#include "../header/add.hpp"
+#include "../header/pow.hpp"
+#include "../header/op.hpp"
+#include "../header/sub.hpp"
+#include "../header/mult.hpp"
+#include "../header/div.hpp"
+#include "../header/rand.hpp"
+#include "../header/factory.hpp"
 
+TEST(TestsRdudh001, OnlyOp) {
+	Factory test;
+	char* args[] = {3};
+	Base* op = test.parse(args,1);
+	
+	EXPECT_DOUBLE_EQ(op->evaluate(), 3);
+}
 
 #endif
--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -1,15 +1,4 @@
 #include "gtest/gtest.h"
-
-#include "../header/base.hpp"
-#include "../header/add.hpp"
-#include "../header/pow.hpp"
-#include "../header/op.hpp"
-#include "../header/sub.hpp"
-#include "../header/mult.hpp"
-#include "../header/div.hpp"
-#include "../header/rand.hpp"
-#include "../header/factory.hpp"
-
 #include "rdudh001_tests.hpp"
 
 int main(int argc, char **argv) {
